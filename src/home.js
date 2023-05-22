9ecca3fb289fd3bb6f2c5054947ce6a4f8708037
cheezyfez <127417678+cheezyfez@users.noreply.github.com>
2023-05-22 14:44:50 +0300

    added anew variable

--- src/home.js
+++ src/home.js
@@ -1,18 +1,16 @@
 const Home = () => {
-
-    const handleClick = (e) => {
-        console.log('Hello Ninjas', e)
+    let name = 'Mario';
+    
+    const handleClick = () => {
+        
     }
 
-    const handleClickAgain = (name) => {
-        console.log('Hello ' + name);
-    }
+    
 
     return (  
         <div className="home">
             <h2>Home page</h2>
             <button onClick={handleClick}>Click Me</button>
-            <button onClick={() => handleClickAgain('Mario')}>Click me again</button>
         </div>
     );
 }
