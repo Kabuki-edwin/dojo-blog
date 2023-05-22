180a0ec4642212ae8c9e3650cba42cd9c65bb96d
cheezyfez <127417678+cheezyfez@users.noreply.github.com>
2023-05-22 11:18:49 +0300

    imported home component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Navbar from "./Navbar";
+import Home from "./home"
 
 function App() {
   const tittle = 'Welcome to the new blog';
--- src/home.js
+++ src/home.js
@@ -1,6 +1,8 @@
 const Home = () => {
     return (  
-        
+        <div className="home">
+            <h2>Home page</h2>
+        </div>
     );
 }
  
