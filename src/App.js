461319f922379031c5b70eea47b9bafc8a89edf1
cheezyfez <127417678+cheezyfez@users.noreply.github.com>
2023-05-22 10:54:26 +0300

    added agoogle link

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
 
 function App() {
+  const tittle = 'Welcome to the new blog';
+  const likes = 50;
+  const link = "https://www.google.com"
+
   return (
     <div className="App">
       <div className="content">
-        <h1>App component</h1>
+        <h1>{ tittle }</h1>
+        <p>Liked { likes } times</p>
+
+        <a href={link}>Google site</a>
       </div>
     </div>
   );
