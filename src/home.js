d4878531799f97cf006e36da4120afa7f960d258
cheezyfez <127417678+cheezyfez@users.noreply.github.com>
2023-05-22 17:52:17 +0300

    added ap element

--- src/home.js
+++ src/home.js
@@ -1,8 +1,8 @@
 const Home = () => {
     let name = 'Mario';
-    
+
     const handleClick = () => {
-        
+        name = 'luigi';
     }
 
     
@@ -10,6 +10,7 @@ const Home = () => {
     return (  
         <div className="home">
             <h2>Home page</h2>
+            <p>{name}</p>
             <button onClick={handleClick}>Click Me</button>
         </div>
     );
