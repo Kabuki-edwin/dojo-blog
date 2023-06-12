248e9d6c14c10cf59429230f487851b16e3bbf7c
cheezyfez <127417678+cheezyfez@users.noreply.github.com>
2023-06-12 12:03:57 +0300

    conditional loading message

--- src/home.js
+++ src/home.js
@@ -3,6 +3,7 @@ import Bloglist from "./Bloglist ";
 
 const Home = () => {
     const [blogs, setBlogs] = useState(null);
+    const [isPending, setIsPending] = useState(true);
 
 useEffect(() => {
     fetch('http://localhost:8000/blogs')
@@ -11,11 +12,13 @@ useEffect(() => {
     })
     .then (data => {
         setBlogs(data);
+        setIsPending(false);
     })
 },[]);
 
     return (  
         <div className="home">
+            { isPending && <div>Loading...</div> }
            {blogs && <Bloglist blogs ={blogs} title ='All blogs'></Bloglist> } 
         </div>
     );
